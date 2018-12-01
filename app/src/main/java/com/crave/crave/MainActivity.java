133574281f3988bbf48afcce3d73553b8dca2973
tobyzl2 <tobyzl2@illinois.edu>
2018-12-01 16:10:33 -0600

    Working on home screen.

--- app/src/main/java/com/crave/crave/MainActivity.java
+++ app/src/main/java/com/crave/crave/MainActivity.java
@@ -2,6 +2,7 @@ package com.crave.crave;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SearchView searchView = findViewById(R.id.home_search_bar);
+        searchView.setQueryHint("What are you craving?");
+
     }
 }
