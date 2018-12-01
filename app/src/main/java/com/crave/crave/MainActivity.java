db8e5b51d7db26bbe1948d17ce0ad637dd8fd00c
tobyzl2 <tobyzl2@illinois.edu>
2018-12-01 16:29:54 -0600

    Finished first home and options.

--- app/src/main/java/com/crave/crave/MainActivity.java
+++ app/src/main/java/com/crave/crave/MainActivity.java
@@ -1,17 +1,31 @@
 package com.crave.crave;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
-
+    MainActivity thisActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        thisActivity = this;
         SearchView searchView = findViewById(R.id.home_search_bar);
         searchView.setQueryHint("What are you craving?");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Intent toOptions = new Intent(thisActivity, Options.class);
+                startActivity(toOptions);
+                return true;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
     }
 }
