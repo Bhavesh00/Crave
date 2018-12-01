b53e6e153ffbcb31154e2aa7fb4d59a17bbcc1b8
Bhavesh00 <bhavesh.manivannan@gmail.com>
2018-12-01 17:40:33 -0600

    Added dependencies.

--- app/src/main/java/com/crave/crave/FindNearby.java
+++ app/src/main/java/com/crave/crave/FindNearby.java
@@ -4,11 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
 public class FindNearby extends AppCompatActivity {
+    private static RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +33,7 @@ public class FindNearby extends AppCompatActivity {
         final String searchVal = getIntent().getStringExtra("SearchValue");
         TextView searchValue = findViewById(R.id.search_val);
         searchValue.setText(searchVal);
+        startAPICall(searchVal);
 
         final FindNearby thisActivity = this;
         SearchView searchView = findViewById(R.id.find_nearby_search_bar);
@@ -46,4 +63,28 @@ public class FindNearby extends AppCompatActivity {
             }
         });
     }
+
+    /* Make API Call */
+    void startAPICall(String searchVal) {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
