b591c1dfc89fa2debac110bd7dd04e582a84e147
tobyzl2 <tobyzl2@illinois.edu>
2018-12-06 20:01:02 -0600

    UI and FindNearby.

--- app/src/main/java/com/crave/crave/MakeYourself.java
+++ app/src/main/java/com/crave/crave/MakeYourself.java
@@ -1,15 +1,35 @@
 package com.crave.crave;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MakeYourself extends AppCompatActivity {
-
+    private static RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +38,7 @@ public class MakeYourself extends AppCompatActivity {
         TextView searchValue = findViewById(R.id.search_val);
         searchValue.setText(searchVal);
 
+        requestQueue = Volley.newRequestQueue(this);
         final MakeYourself thisActivity = this;
         SearchView searchView = findViewById(R.id.make_yourself_search_bar);
         searchView.setQueryHint("What else are you craving?");
@@ -45,5 +66,40 @@ public class MakeYourself extends AppCompatActivity {
                 startActivity(toFindNearby);
             }
         });
+    }
+    /* Make API Call */
+    void startAPICall(String searchVal, double longit, double latit) {
+        String url = "https://api.edamam.com/search";
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                    url,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Toast.makeText(getApplicationContext(), "Response!", Toast.LENGTH_LONG).show();
+                            Log.d("response_JSON", "" + response);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    error.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Error:" + error.toString(), Toast.LENGTH_LONG).show();
+                }
+            }){
+                @Override
+                public Map<String, String> getHeaders() {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("user-key", "a7d51f7199c934dd65a3207602073127");
+                    params.put("Accept", "application/json");
+                    return params;
+                }
+            };
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
